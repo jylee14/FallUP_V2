9bef9b18b03d277f6a4d41215563cf26993511d8
Jun <jy45lee@gmail.com>
2017-08-14 20:11:14 -0700

    initial commit + assets + initial map set up

--- FallUP/GameScene.swift
+++ FallUP/GameScene.swift
@@ -9,79 +9,106 @@
 import SpriteKit
 import GameplayKit
 
+struct CollisionMasks{
+    static let ball:UInt32 = 0x1 << 1
+    static let wall:UInt32 = 0x1 << 2
+    static let bomb:UInt32 = 0x1 << 3
+    static let badWall:UInt32 = 0x1 << 4
+}
+
 class GameScene: SKScene {
+    private var ball: SKSpriteNode?  //player's character
+    private var bomb: SKSpriteNode?  //miniature squares that the player needs to avoid
+    private var topWall: SKSpriteNode?   //top wall
+    private var botWall: SKSpriteNode?   //bottom wall
+    private var badWall: SKSpriteNode?   //bad wall that player needs to avoid
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var isBlue = true
+    
+    /**
+     default func provided by XCode
+     override func didMove(to view: SKView) {
+     
+     // Get label node from scene and store it for use later
+     self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+     if let label = self.label {
+     label.alpha = 0.0
+     label.run(SKAction.fadeIn(withDuration: 2.0))
+     }
+     
+     // Create shape node to use during mouse interaction
+     let w = (self.size.width + self.size.height) * 0.05
+     self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+     
+     if let spinnyNode = self.spinnyNode {
+     spinnyNode.lineWidth = 2.5
+     
+     spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+     spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+     SKAction.fadeOut(withDuration: 0.5),
+     SKAction.removeFromParent()]))
+     }
+     }
+     */
     
     override func didMove(to view: SKView) {
+        ball = self.childNode(withName: "//ball") as? SKSpriteNode
+        ball?.physicsBody?.categoryBitMask = CollisionMasks.ball
+        ball?.physicsBody?.collisionBitMask = CollisionMasks.wall | CollisionMasks.badWall | CollisionMasks.bomb
+        ball?.physicsBody?.contactTestBitMask = CollisionMasks.wall | CollisionMasks.badWall | CollisionMasks.bomb
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        topWall = self.childNode(withName: "//topWall") as? SKSpriteNode
+        topWall?.physicsBody?.categoryBitMask = CollisionMasks.wall
+        topWall?.physicsBody?.collisionBitMask = CollisionMasks.ball
+        topWall?.physicsBody?.contactTestBitMask = CollisionMasks.ball
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        botWall = self.childNode(withName: "//botWall") as? SKSpriteNode
+        botWall?.physicsBody?.categoryBitMask = CollisionMasks.wall
+        botWall?.physicsBody?.collisionBitMask = CollisionMasks.ball
+        botWall?.physicsBody?.contactTestBitMask = CollisionMasks.ball
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    private func addBall(){
+        ball = SKSpriteNode(imageNamed: "blueBall")
+        ball?.size = CGSize(width: 60, height: 60)
+        ball?.position = CGPoint(x: (self.scene?.size.width)!/4, y: (self.scene?.size.height)! * 3/4)
+        ball?.physicsBody = SKPhysicsBody(circleOfRadius: 30)
+        ball?.physicsBody?.isDynamic = false
+        ball?.physicsBody?.affectedByGravity = false
+        
+        self.addChild(ball!)
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    private  func addTopAndBottomWalls(){
+        topWall = SKSpriteNode(imageNamed: "blueWall")
+        topWall?.position = CGPoint(x: self.frame.width/2, y: self.frame.height - (topWall?.size.height)!)
+        topWall?.physicsBody = SKPhysicsBody(rectangleOf: (topWall?.size)!)
+        topWall?.physicsBody?.isDynamic = false
+        topWall?.physicsBody?.affectedByGravity = false
+        
+        botWall = SKSpriteNode(imageNamed: "orangeWall")
+        botWall?.position = CGPoint(x: self.frame.width/2, y: self.frame.height - (topWall?.size.height)!)
+        botWall?.physicsBody = SKPhysicsBody(rectangleOf: (botWall?.size)!)
+        botWall?.physicsBody?.isDynamic = false
+        botWall?.physicsBody?.affectedByGravity = false
+        
+        self.addChild(topWall!)
+        self.addChild(botWall!)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if isBlue{
+            ball?.texture = SKTexture(imageNamed: "orangeBall")
+            self.physicsWorld.gravity = CGVector(dx: 0.0, dy: 5.0)
+            isBlue = false
+        }else{
+            ball?.texture = SKTexture(imageNamed: "blueBall")
+            self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -5.0)
+            isBlue = true
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
--- FallUP/GameViewController.swift
+++ FallUP/GameViewController.swift
@@ -14,7 +14,6 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
@@ -25,31 +24,13 @@ class GameViewController: UIViewController {
                 view.presentScene(scene)
             }
             
-            view.ignoresSiblingOrder = true
-            
+            //view.ignoresSiblingOrder = true
             view.showsFPS = true
             view.showsNodeCount = true
         }
     }
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    override var shouldAutorotate: Bool { return false }
+    override var prefersStatusBarHidden: Bool { return true }
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask { return .portrait }
 }
